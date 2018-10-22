902354052f7ad15bf97a1f76d769f6400c608193
masitings <masitingss@gmail.com>
2018-10-22 20:48:43 +0700

    Indonesian Language (Fix)

---
+++ resources/lang/id/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reminder Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines which match reasons
+	| that are given by the password broker for a password update attempt
+	| has failed, such as for an invalid token or invalid new password.
+	|
+	*/
+
+	"password" => "Password harus mengandung minimal 6 karater dan sesuai dengan konfirmasi.",
+	"user" => "Kami tidak dapat menemukan user dengan alamat email tersebut",
+	"token" => "Token password reset invalid!",
+	"sent" => "Kami telah mengirimkan link reset password pada email anda",
+	"reset" => "Password anda telah diubah!",
+
+];
